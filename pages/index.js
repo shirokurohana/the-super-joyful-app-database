5ab598c3b572f31d7c1352019387affd02968ebd
Nathalie Baladejo-Reynosa <nathy.reynosa@protonmail.com>
2021-09-15 23:05:06 +0000

    trying to fix Error: Missing key prop for element in iterator  react/jsx-key

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,15 @@ export default function Home({ allData }) {
           <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
         </svg>
         </p>
+
         <div className="text-center">
-          {allData.map(({ id, name }) => (
-            <ul className="list-group list-group-flush">
-                <Link key={id} href={`/${id}`}>
-                  <a className="list-group-item">{name} </a> 
-                </Link>
-            </ul>
-          ))}
+          <ul className="list-group list-group-flush">
+            {allData.map(({ id, name }) => (
+              <Link key={id} href={`/${id}`}>
+                <a className="list-group-item list-group-item-action">{name}</a>
+              </Link>
+            ))}
+          </ul>
         </div>
       </Layout>
   );
